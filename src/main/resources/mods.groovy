a293c4d94e9c49f706191f2949878e032cccc469
Luke Bemish <lukebemish@lukebemish.dev>
2024-04-24 17:10:44 -0500

    More work on 1.20.5 port

--- src/main/resources/mods.groovy
+++ src/main/resources/mods.groovy
@@ -1,71 +1,52 @@
-import modsdotgroovy.Dependency
-
-ModsDotGroovy.make {
+MultiplatformModsDotGroovy.make {
     modLoader = 'javafml'
     loaderVersion = '[1,)'
-
     license = 'LGPL-3.0-or-later'
     issueTrackerUrl = 'https://github.com/lukebemishprojects/DynamicAssetGenerator/issues'
 
     mod {
-        modId = this.buildProperties['mod_id']
-        displayName = this.buildProperties['mod_name']
-        version = this.version
+        modId = buildProperties.mod_id
+        displayName = buildProperties.mod_name
+        version = environmentInfo.version
         displayUrl = 'https://github.com/lukebemishprojects/DynamicAssetGenerator'
+        contact {
+            sources = 'https://github.com/lukebemishprojects/DynamicAssetGenerator'
+        }
+        author = 'Luke Bemish'
+        description = buildProperties.description
 
-        description = 'A tool for generating asset and data resources at runtime from existing resources.'
-        authors = [this.buildProperties['mod_author'] as String]
+        entrypoints {
+            client = 'dev.lukebemish.dynamicassetgenerator.impl.fabriquilt.fabric.DynamicAssetGeneratorClientFabric'
+            main = 'dev.lukebemish.dynamicassetgenerator.impl.fabriquilt.fabric.DynamicAssetGeneratorFabric'
+        }
 
         dependencies {
             mod 'minecraft', {
-                def minor = this.libs.versions.minecraft.split(/\./)[1] as int
-                versionRange = "[${this.libs.versions.minecraft},1.${minor+1}.0)"
+                versionRange = ">=${libs.versions.minecraft}"
             }
-
-            onForge {
-                neoforge = ">=${this.libs.versions.neoforge}"
+            onNeoForge {
+                neoforge = ">=${libs.versions.neoforge}"
             }
-
             onFabric {
                 mod 'fabricloader', {
-                    versionRange = ">=${this.libs.versions.fabric.loader}"
+                    versionRange = ">=${libs.versions.fabric_loader}"
                 }
                 mod 'fabric-api', {
-                    versionRange = ">=${this.libs.versions.fabric.api.split(/\+/)[0]}"
+                    versionRange = ">=${libs.versions.fabric_api.split(/\+/)[0]}"
                 }
-            }
-        }
-
-            onForge {
-                dependencies = dependencies.collect { dep ->
-                    new Dependency() {
-                        @Override
-                        Map asForgeMap() {
-                            def map = dep.asForgeMap()
-                            map.remove('mandatory')
-                            map.put('type', this.mandatory ? 'required' : 'optional')
-                            return map
-                        }
-                    }
-                }
-            }
-
-        entrypoints {
-            onFabric {
-                entrypoint 'client', ['dev.lukebemish.dynamicassetgenerator.impl.fabriquilt.fabric.DynamicAssetGeneratorClientFabric']
-                entrypoint 'main', ['dev.lukebemish.dynamicassetgenerator.impl.fabriquilt.fabric.DynamicAssetGeneratorFabric']
             }
         }
     }
+
     onFabric {
-        mixin = [
-            'mixin.dynamic_asset_generator.json',
-            'mixin.dynamic_asset_generator_fabriquilt.json'
-        ]
+        mixins {
+            mixin 'mixin.dynamic_asset_generator.json'
+            mixin 'mixin.dynamic_asset_generator_fabriquilt.json'
+        }
     }
-    onForge {
-        mixins = [
-            ['config':'mixin.dynamic_asset_generator.json']
-        ]
+    onNeoForge {
+        mixins {
+            mixin 'mixin.dynamic_asset_generator.json'
+        }
     }
 }
