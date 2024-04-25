a6e3d9698ca0ad7acf0898e34b26a6e7eeecc073
Luke Bemish <lukebemish@lukebemish.dev>
2024-04-24 21:31:26 -0500

    Neoforge compiles

--- src/neoforge/java/dev/lukebemish/dynamicassetgenerator/impl/neoforge/EventHandler.java
+++ src/neoforge/java/dev/lukebemish/dynamicassetgenerator/impl/neoforge/EventHandler.java
@@ -3,6 +3,8 @@ package dev.lukebemish.dynamicassetgenerator.impl.neoforge;
 import dev.lukebemish.dynamicassetgenerator.impl.DynamicAssetGenerator;
 import dev.lukebemish.dynamicassetgenerator.impl.GeneratedPackResources;
 import net.minecraft.network.chat.Component;
+import net.minecraft.server.packs.PackLocationInfo;
+import net.minecraft.server.packs.PackSelectionConfig;
 import net.minecraft.server.packs.PackType;
 import net.minecraft.server.packs.repository.Pack;
 import net.minecraft.server.packs.repository.PackCompatibility;
@@ -11,6 +13,7 @@ import net.minecraft.world.flag.FeatureFlagSet;
 import net.neoforged.neoforge.event.AddPackFindersEvent;
 
 import java.util.List;
+import java.util.Optional;
 
 public class EventHandler {
     public static void addResourcePack(AddPackFindersEvent event) {
@@ -20,22 +23,19 @@ public class EventHandler {
             if (info.cache().getPackType() == type) {
                 event.addRepositorySource(consumer -> {
                     var metadata = DynamicAssetGenerator.makeMetadata(info.cache());
-                    var packInfo = new Pack.Info(
+                    var packMetadata = new Pack.Metadata(
                         metadata.description(),
                         PackCompatibility.COMPATIBLE,
                         FeatureFlagSet.of(),
                         List.of(),
                         true
                     );
-                    Pack pack = Pack.create(
-                        DynamicAssetGenerator.MOD_ID + '/' + info.cache().getName(),
-                        Component.literal(DynamicAssetGenerator.MOD_ID+'/'+ info.cache().getName()),
-                        true,
+                    var id = DynamicAssetGenerator.MOD_ID+'/'+ info.cache().getName();
+                    Pack pack = new Pack(
+                        new PackLocationInfo(id, Component.literal(id), PackSource.DEFAULT, Optional.empty()),
                         new GeneratedPackResources.GeneratedResourcesSupplier(info.cache()),
-                        packInfo,
-                        info.position(),
-                        true,
-                        PackSource.DEFAULT
+                        packMetadata,
+                        new PackSelectionConfig(true, info.position(), true)
                     );
                     consumer.accept(pack);
                 });
